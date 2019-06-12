2867256eb000e60d36a276ad7587f3ddd681e4a0
Ryan Wilson <wilsonryan@google.com>
2019-06-12 12:48:41 -0400

    Update incorrect module name in import.
    This was pointed out by Matt Thomas [in a comment on SR-10707](https://bugs.swift.org/browse/SR-10707?focusedCommentId=47262&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-47262).

--- Sources/ObjCBar/RWBar.m
+++ Sources/ObjCBar/RWBar.m
@@ -1,12 +1,12 @@
 #import "include/RWBar.h"
 
-// BROKEN: Importing a module that I would expect to exist (RWFoo) does not work. Two ways:
+// BROKEN: Importing a module that I would expect to exist (ObjCFoo) does not work. Two ways:
 //   1. fatal error: module 'RWFoo' not found. I would expect that the module `RWFoo` exists since
 //      it imports property into the Swift target (spm-module-repro).
 //
 //   2. Xcode generated project error: `Use of '@import' when modules are disabled. The target
 //      generated in Xcode doesn't have modules enabled for C and Objective-C.
-@import RWFoo;
+@import ObjCFoo;
 
 // Importing header by filename directly from a different target works (slightly unexpected).
 // UNCOMMENT THIS IMPORT TO FIX THE BUILD.
